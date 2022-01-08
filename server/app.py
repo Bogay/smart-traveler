f23c25c039b0cbb7fc3fb9db6c251f18958abdaa
bogay <pojay11523@gmail.com>
2022-01-08 19:41:20 +0800

    docs: add some comments

--- server/app.py
+++ server/app.py
@@ -3,17 +3,22 @@ from flask import Flask, jsonify, request
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-app = Flask(__name__)
+# Setup scheduler to crawl country data every 1 min.
 scheduler = BackgroundScheduler()
 intervalTrigger = IntervalTrigger(minutes=1)
 scheduler.add_job(process_data, intervalTrigger, id='crawl')
 scheduler.start()
 
+app = Flask(__name__)
 
+
+# Register `GET /`
 @app.get('/')
 def post():
     country = request.values.get('country')
+    # Query country data by given country name
     r = country_data.get(country, None)
+    # Return 404 if the name not in dictionary
     if r is None:
         status = 404
     else:
@@ -22,4 +27,5 @@ def post():
 
 
 if __name__ == '__main__':
+    # Start servere and listen 5000 port on any address
     app.run(host='0.0.0.0', port='5000')
